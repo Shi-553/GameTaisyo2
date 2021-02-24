76d41df4b85e4994ff89786d49f42b971bf43b6c
HALAT12G05 <74965607+HALAT12G05@users.noreply.github.com>
2021-02-24 18:30:14 +0900

    Hummer Door を追加しました。

---
+++ Assets/Scripts/Hummer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hummer : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        var o = other.GetComponent<IOperatedHummerObject>();
+        if (o != null)
+        {
+            o.Hit();
+        }
+    }
+
+}
