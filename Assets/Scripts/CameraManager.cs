08b23f168bf3c8471b661660adece61d38608c27
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-02-25 20:59:51 +0900

    カメラの改良

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -8,43 +8,63 @@ public class CameraManager : MonoBehaviour {
 
     [SerializeField] float speed = 1;
 
-    Vector3 befPoint;
-    Vector3 befN;
     // Start is called before the first frame update
     void Start() {
 
-        befPoint = transform.up;
-        befN = -transform.forward;
-    }
-
-    void Update() {
-
 
         var forwerdRay = new Ray(transform.position + transform.forward * 5, transform.forward);
+        if (Physics.Raycast(forwerdRay, out var forwerdHit, Mathf.Infinity, mask)) {
+            var forwardPoints = PointDistance.GetUpRight(forwerdHit, transform.up, transform.right);
 
+            var point = ((forwardPoints.LeftTop + forwardPoints.RightTop) / 2 +
+                (forwardPoints.LeftBottom + forwardPoints.RightBottom) / 2) / 2;
+
+
+            transform.position = point + forwardPoints.Normal * 10;
+
+            transform.LookAt(point, forwardPoints.Up.normalized);
+            aftQ = transform.up;
+        }
+    }
+    Vector3 aftQ;
+    void Update() {
+        //transform.rotation = aftQ;
+
+        var forwerdRay = new Ray(transform.position + transform.forward * 5, transform.forward);
 
 
         if (Physics.Raycast(forwerdRay, out var forwerdHit, Mathf.Infinity, mask)) {
             var forwardPoints = PointDistance.GetUpRight(forwerdHit, transform.up, transform.right);
 
-            Debug.DrawRay(forwerdHit.point, forwardPoints.Normal * 10,Color.red);
+            Debug.DrawRay(forwerdHit.point, forwardPoints.Normal * 10, Color.red);
             Debug.DrawRay(forwerdHit.point, forwardPoints.Normal1 * 10);
             Debug.DrawRay(forwerdHit.point, forwardPoints.Normal2 * 10);
 
-            var point = (forwardPoints.LeftTop + forwardPoints.LeftBottom) / 2;
+
+            transform.position = Vector3.Lerp(transform.position, forwerdHit.point + forwardPoints.Normal * 10,0.01f);
+
+            var firstCenter = forwardPoints.LeftTop + forwardPoints.LeftBottom / 2;
+            var lastCenter = forwardPoints.RightTop + forwardPoints.RightBottom / 2;
+
+            transform.position = Vector3.Lerp(transform.position, forwerdHit.point + forwardPoints.Normal * 10, 0.01f);
+            // forwerdRay = new Ray(transform.position + transform.forward * 5, transform.forward);
+            // if (Physics.Raycast(forwerdRay, out forwerdHit, Mathf.Infinity, mask)) {
+            //      forwardPoints = PointDistance.GetUpRight(forwerdHit, transform.up, transform.right);
+            //
+            // var befQ = transform.rotation;
+            aftQ =Vector3.Lerp(aftQ, forwardPoints.Up.normalized,0.01f);
+
+            transform.LookAt(forwerdHit.point, aftQ);
+            // aftQ = transform.rotation;
+
+            // }
+
+            var add = ((forwardPoints.TopWidth) +
+                (forwardPoints.BottomWidth)) / 2;
+            transform.position += add.normalized * speed;
 
 
-            transform.position = Vector3.Slerp(transform.position, point + forwardPoints.Normal * 10, 0.01f);
-
-
-            befPoint = Vector3.Lerp(befPoint, forwardPoints.LeftHeight.normalized, 0.005f);
-
-            transform.LookAt(forwerdHit.point, befPoint);
-
-
-
-            transform.position += forwardPoints.LonggerWidth.normalized * speed;
-
+            // transform.rotation = Quaternion.Lerp(befQ, aftQ, 0.01f);
         }
     }
 }
