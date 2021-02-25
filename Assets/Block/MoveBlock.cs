99ab50ec42d1b3c4784dd112332733e98ce5f0c7
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-02-25 11:53:59 +0900

    壁に沿ってうごくようにした

--- Assets/Block/MoveBlock.cs
+++ Assets/Block/MoveBlock.cs
@@ -2,18 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveBlock : MonoBehaviour
-{
+public class MoveBlock : MonoBehaviour {
+    int mask;
+
     [SerializeField] TransformBlock TransformBlock;
-    Vector3 firstPosition;
-    private void Start()
-    {
-        firstPosition = transform.localPosition;
+    [SerializeField] [Range(0.0f,1.0f)] float smoothSpeed=0.1f;
+
+
+    Vector3 prevAnimeValue;
+
+    private void Start() {
+        mask = LayerMask.GetMask(new string[] { "Mebiusu" });
+        prevAnimeValue = TransformBlock.GetCurrentAnimeValue();
+
+        Set(Vector3.zero, prevAnimeValue);
     }
 
-    private void Update()
-    {
-        transform.localPosition = (firstPosition+TransformBlock.GetCurrentAnimeValue());
+    private void Update() {
+
+        var animeValue = TransformBlock.GetCurrentAnimeValue();
+        Set(prevAnimeValue, animeValue);
+        prevAnimeValue = animeValue;
     }
 
+    void Set(Vector3 prev, Vector3 current) {
+        Ray forwerdRay = new Ray(transform.position - transform.forward, transform.forward);
+
+
+        Debug.DrawRay(forwerdRay.origin, forwerdRay.direction * 10);
+
+        if (Physics.Raycast(forwerdRay, out var forwerdHit, Mathf.Infinity, mask)) {
+
+            var forwardPoints = PointDistance.GetUpRight(forwerdHit, transform.up, transform.right);
+
+
+            var sa = current - prev;
+
+            transform.localPosition += forwardPoints.LonggerWidth.normalized * sa.x + 
+                forwardPoints.LeftHeight.normalized * sa.y+
+                forwardPoints.Normal * sa.z;
+        }
+
+        forwerdRay = new Ray(transform.position - transform.forward, transform.forward);
+
+        if (Physics.Raycast(forwerdRay, out forwerdHit, Mathf.Infinity, mask)) {
+
+            var forwardPoints = PointDistance.GetUpRight(forwerdHit, transform.up, transform.right);
+
+
+            Vector3 upFV = forwardPoints.LeftHeight.normalized;
+
+            Quaternion OriginalRot = transform.rotation;
+
+            transform.LookAt(transform.position - forwardPoints.Normal, upFV);
+            Quaternion NewRot = transform.rotation;
+
+            transform.rotation = Quaternion.Slerp(OriginalRot, NewRot, smoothSpeed);
+        }
+    }
 }
+
+
--- Assets/Scripts/TransformBlock.cs
+++ Assets/Scripts/TransformBlock.cs
@@ -6,16 +6,20 @@ public class TransformBlock
 {
     [SerializeField] Vector3 beginValue;
     [SerializeField] Vector3 endValue;
+    [SerializeField] float speed=1;
     [SerializeField] AnimationCurve curve;
     // Start is called before the first frame update
 
     public Vector3 GetCurrentAnimeValue()
     {
 
-        var time = curve.Evaluate(Time.time);
+        var time = GetCurrentTime();
 
        return  beginValue* (1 - time) + endValue* time;
 
+    }
+    public float GetCurrentTime() {
+        return curve.Evaluate(Time.time* speed); ;
     }
 
 }
