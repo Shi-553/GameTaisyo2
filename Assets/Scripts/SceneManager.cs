34dc0c540983ab325f488fcce66181367fa4f876
sisyamo2236@gmail.com <sisyamo2236@gmail.com>
2021-03-03 16:30:11 +0900

    scenemanagerの追加

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public enum SceneType
+{
+    TITLE,
+    GAME,
+    RESULT,
+};
+
+namespace Scene
+{
+
+    public class SceneManager : MonoBehaviour
+    {
+        public void ChangeScene(int SceneType, int LoadSceneMode)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(SceneType, (LoadSceneMode)LoadSceneMode);
+        }
+    }
+}
+
