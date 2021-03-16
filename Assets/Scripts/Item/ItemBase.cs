edcc9719a4a08adc8f808509d580c46655683523
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-03-16 15:40:12 +0900

    アイテムを追加

---
+++ Assets/Scripts/Item/ItemBase.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Item {
+    public abstract class ItemBase : MonoBehaviour {
+        protected void Delete() {
+            Destroy(gameObject);
+        }
+    }
+    public abstract class ImmediateItemBase : ItemBase {
+        public abstract void Hit(GameObject player);
+    }
+    public abstract class UseableItemBase : ItemBase {
+        public abstract void Use(GameObject player);
+    }
+}
