d4c3f8d53ef84d7d96e2e8a14bde2474fe048074
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-03-16 19:08:46 +0900

    ブロックを整えるエディタ拡張を追加

---
+++ Assets/Editor/StageEditor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public class StageEditor
+{
+    [MenuItem("Tools/SetBlock")]
+    static void SetBlock() {
+        Undo.RecordObjects(Selection.gameObjects.Select(o=>o.transform).ToArray(), "set");
+        var mask = LayerMask.GetMask(new string[] { "Mebiusu" });
+
+
+        foreach (var gameObject in Selection.gameObjects) {
+            var transform = gameObject.transform;
+
+            var forwerdRay = new Ray(transform.position - transform.forward, transform.forward);
+
+            if (Physics.Raycast(forwerdRay, out var forwerdHit, Mathf.Infinity, mask)) {
+
+                var forwardPoints = PointDistance.GetUpRight(forwerdHit, transform.up, transform.right);
+
+
+                Vector3 upFV = forwardPoints.Up.normalized;
+
+
+                transform.LookAt(transform.position - forwardPoints.Normal, upFV);
+            }
+
+
+             forwerdRay = new Ray(transform.position - transform.forward, transform.forward);
+
+
+
+            if (Physics.Raycast(forwerdRay, out  forwerdHit, Mathf.Infinity, mask)) {
+
+                var forwardPoints = PointDistance.GetUpRight(forwerdHit, transform.up, transform.right);
+
+
+
+                transform.localPosition = forwerdHit.point+ forwardPoints.Normal *2;
+            }
+
+        }
+    }
+}
