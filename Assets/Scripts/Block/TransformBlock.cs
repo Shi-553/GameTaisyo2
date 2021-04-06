5b835e8f106cdf9cd9c9251321071fbdc84fb450
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-04-06 20:18:33 +0900

    もう一回

--- Assets/Scripts/Block/TransformBlock.cs
+++ Assets/Scripts/Block/TransformBlock.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TransformBlock : MonoBehaviour,Item.TimeStopable {
+public class TransformBlock : MonoBehaviour, Item.TimeStopable {
     [SerializeField] protected Vector3 beginValue;
-    [SerializeField]protected Vector3 endValue;
+    [SerializeField] protected Vector3 endValue;
     [SerializeField] protected float speed = 1;
     [SerializeField] protected AnimationCurve curve;
 
     public bool isStopped = false;
 
-    float stoppedTime=0;
-    float allDiffTime=0;
+    float stoppedTime = 0;
+    float allDiffTime = 0;
 
 
     public Vector3 GetCurrentAnimeValue() {
@@ -24,15 +24,21 @@ public class TransformBlock : MonoBehaviour,Item.TimeStopable {
     public float GetCurrentTime() {
         if (isStopped) {
             if (stoppedTime == 0) {
-                stoppedTime = Time.time;
+                TimeStopped();
             }
-                return curve.Evaluate((stoppedTime- allDiffTime) * speed);
+            return curve.Evaluate((stoppedTime - allDiffTime) * speed);
+
         }
-        return curve.Evaluate((Time.time - allDiffTime) * speed);
+        else {
+            if (stoppedTime != 0) {
+                TimeReStarted();
+            }
+            return curve.Evaluate((Time.time - allDiffTime) * speed);
+        }
     }
 
     public void TimeReStarted() {
-        allDiffTime +=Time.time-stoppedTime;
+        allDiffTime += Time.time - stoppedTime;
         stoppedTime = 0;
         isStopped = false;
     }
