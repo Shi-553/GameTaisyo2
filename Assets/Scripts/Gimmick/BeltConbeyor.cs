1f2b0fdf7c79b6733bd6d7495f24db2f993cb6f2
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-04-20 20:37:50 +0900

    ベルトコンベア（瞬間移動）実装

---
+++ Assets/Scripts/Gimmick/BeltConbeyor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class BeltConbeyor : MonoBehaviour
+{
+    public void Activation() {
+        var colliders = Physics.OverlapBox(transform.position, transform.localScale / 2, transform.rotation);
+        var objects = colliders.Where(c => !c.CompareTag("Mebiusu")|| !c.CompareTag("Player"));
+        foreach (var obj in objects) {
+            var trans = obj.transform;
+            var pos = trans.localPosition;
+            pos.z *= -1;
+            var angle=trans.localEulerAngles;
+            angle.x +=180;
+            trans.localEulerAngles = angle;
+            trans.localPosition = pos;
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
