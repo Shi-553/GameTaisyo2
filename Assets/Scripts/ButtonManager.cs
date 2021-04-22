85a76768f1ee74d130c96ec36b8a14deb6aaa483
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-04-22 21:21:20 +0900

    ボタンを上下左右対応にした

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -3,49 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ButtonManager : MonoBehaviour
-{
+public class ButtonManager : MonoBehaviour {
     [SerializeField]
     Transform selectImage;
     int selectIndex = 0;
     bool pressed = false;
 
+    [SerializeField] bool swapVH = false;
 
-    void Update()
-    {
+    [SerializeField] bool reverseV = false;
+    [SerializeField] bool reverseH = false;
+
+    [SerializeField] int columnMax = 0;
+
+    void Update() {
         float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
 
-        if(!pressed)
-        {
-            if (v > 0)
-            {
-                selectIndex -= 1;
+        if (swapVH) {
+            var temp = v;
+            v = h;
+            h = temp;
+        }
+
+        if (!pressed) {
+            if (v != 0 || h != 0) {
                 pressed = true;
             }
-            if (v < 0)
-            {
-                selectIndex += 1;
-                pressed = true;
+
+            var vVal = reverseV ? 1 : -1;
+            var hVal = reverseH ? 1 : -1;
+            if (v > 0) {
+                selectIndex -= 1* vVal;
+            }
+            if (v < 0) {
+                selectIndex += 1 * vVal;
+            }
+            if (h > 0) {
+                selectIndex -= columnMax * hVal;
+            }
+            if (h < 0) {
+                selectIndex += columnMax * hVal;
             }
         }
-        if(v==0)
-        {
+        if (v == 0 && h == 0) {
             pressed = false;
         }
-        
-        if(selectIndex < 0)
-        {
-            selectIndex = transform.childCount - 1;
+
+        if (selectIndex < 0) {
+            selectIndex = (selectIndex+ transform.childCount) % transform.childCount;
         }
-        if (selectIndex > transform.childCount - 1)
-        {
-            selectIndex = 0;
+        if (selectIndex > transform.childCount - 1) {
+            selectIndex = selectIndex % transform.childCount;
         }
         selectImage.position = transform.GetChild(selectIndex).position;
 
 
-        if(Input.GetButtonDown("Submit"))
-        {
+        if (Input.GetButtonDown("Submit")) {
             UnityEngine.EventSystems.BaseEventData data = new UnityEngine.EventSystems.BaseEventData(UnityEngine.EventSystems.EventSystem.current);
             transform.GetChild(selectIndex).GetComponent<Button>().OnSubmit(data);
         }
