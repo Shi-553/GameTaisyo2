3ebe12997d74f82f636d03fa8e6bfdfee063f65d
sisyamo2236@gmail.com <sisyamo2236@gmail.com>
2021-04-22 17:33:45 +0900

    Timer作成

--- Assets/Scripts/Gimmick/Button.cs
+++ Assets/Scripts/Gimmick/Button.cs
@@ -17,4 +17,16 @@ public class Button : MonoBehaviour
     {
         Scene.SceneManager.Instance.ChangeScene(sceneType, LoadSceneMode.Single);
     }
+
+    public void EndGame()
+    {
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+      UnityEngine.Application.Quit();
+#endif
+        
+    }
+
 }
---
+++ Assets/Timer.cs
@@ -0,0 +1,58 @@
+﻿using Item;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] Text CountText;
+    [SerializeField] float CountTime = 4.0f;
+    int count=1;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        var a = FindObjectsOfType<Component>();
+        foreach (var c in a)
+        {
+            var timeStopable = c as TimeStopable;
+
+            if (timeStopable != null)
+            {
+                timeStopable.TimeStopped();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (count > 0)
+        {
+            count = (int)CountTime;
+            CountText.text = count.ToString();
+            CountTime -= 0.01f;
+            Time.timeScale = 0;
+        }
+        else if (count <= 0)
+        {
+            Time.timeScale = 1;
+            CountText.text = "";
+
+            var a = FindObjectsOfType<Component>();
+            foreach (var c in a)
+            {
+                var timeStopable = c as TimeStopable;
+
+                if (timeStopable != null)
+                {
+                    timeStopable.TimeReStarted();
+                }
+            }
+        }
+    }
+  
+}
