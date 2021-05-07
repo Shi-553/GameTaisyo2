664863f7e71a8d18881dc0bf369ea151445f954a
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-05-07 20:12:44 +0900

    クリア時にカメラが動かないようにした

--- Assets/Scripts/Gimmick/Goal.cs
+++ Assets/Scripts/Gimmick/Goal.cs
@@ -8,7 +8,8 @@ public class Goal : MonoBehaviour, IOperatedPlayerObject
     public void Show() {
         gameObject.SetActive(true);
     }
-    void IOperatedPlayerObject.Hit(){
+    void IOperatedPlayerObject.Hit() {
+        Scene.SceneManager.Instance.TimeStop();
         Scene.SceneManager.Instance.ChangeScene(Scene.SceneType.GAMECLEAR, LoadSceneMode.Additive);
     }
 
