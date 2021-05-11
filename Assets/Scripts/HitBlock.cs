dd8f1b44c1c83c8227822e530bdb06a573d012c2
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-05-11 22:51:27 +0900

    ベルトコンベア使うと動かなくなるバグ修正

--- Assets/Scripts/HitBlock.cs
+++ Assets/Scripts/HitBlock.cs
@@ -58,7 +58,7 @@ public class HitBlock : MonoBehaviour, IOperatedHummerObject
 
             if (dir != Vector2.zero)
             {
-                GetComponent<Rigidbody>().AddForce(move * ( Time.timeScale), ForceMode.VelocityChange);
+                GetComponent<Rigidbody>().AddForce(move, ForceMode.VelocityChange);
 
             }
 
