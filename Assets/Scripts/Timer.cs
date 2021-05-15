f825733732b8ed9bb1d35784734c094e754f06f0
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-05-15 14:26:11 +0900

    BGMがループするように修正

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -9,13 +9,18 @@ public class Timer : MonoBehaviour
     [SerializeField] Text countText;
     [SerializeField] float countTime = 4.0f;
     int count = 1;
+    AudioSource gameBGM;
+
     [SerializeField]
     AudioClip bgm;
+    [SerializeField]
+    AudioClip countDownSE;
 
-    // Start is called before the first frame update
     void Start()
     {
         Scene.SceneManager.Instance.TimeStop();
+        gameBGM = GameObject.Find("GameBGM").GetComponent<AudioSource>();
+        gameBGM.PlayOneShot(countDownSE);
     }
 
     // Update is called once per frame
@@ -35,7 +40,9 @@ public class Timer : MonoBehaviour
 
             gameObject.SetActive(false);
             countText.text = "";
-            AudioManager.Instance.Play(bgm);
+            gameBGM.clip = bgm;
+            gameBGM.loop = true;
+            gameBGM.Play();
             Scene.SceneManager.Instance.TimeRestart();
             count = 0;
         }
