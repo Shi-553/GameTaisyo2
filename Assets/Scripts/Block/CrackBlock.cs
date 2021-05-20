9c0bcfa96174e24d2ff46ca467c38646454929db
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-05-21 03:11:15 +0900

    ボムでもエフェクト追加

--- Assets/Scripts/Block/CrackBlock.cs
+++ Assets/Scripts/Block/CrackBlock.cs
@@ -11,26 +11,34 @@ public class CrackBlock : MonoBehaviour, Damage.IGimmickDamageable, IOperatedHum
     public void ApplyDamage(int damage) {
         if (h == null) {
             if (hp <= damage) {
-                Destroy(gameObject);
+                DrawEffect();
             }
             return;
         }
 
         if (hp <= damage) {
             if (h.ApplyDamage(20)) {
-                gameObject.SetActive(false);
-                var cs = transform.parent.GetComponentsInChildren<ParticleSystem>();
-                foreach (var c in cs) {
-                    c.Play();
-                }
-                
-                Destroy(gameObject.transform.parent.gameObject,2);
+                DrawEffect();
             }
         }
         else {
             h.ApplyDamage(20, false);
         }
         h = null;
+    }
+    void DrawEffect() {
+        foreach (var c in gameObject.GetComponents<Collider>()) {
+            c.enabled = false;
+        }
+        foreach (var c in gameObject.GetComponents<MeshRenderer>()) {
+            c.enabled = false;
+        }
+        var cs = transform.GetComponentsInChildren<ParticleSystem>();
+        foreach (var c in cs) {
+            c.Play();
+        }
+
+        Destroy(gameObject, 2);
     }
 
     public void Hit(PlayerHummer hummer) {
