48886581f7bf2ec05cf400b23567dbe601226741
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-05-20 10:14:50 +0900

    連打を修正

--- Assets/Scripts/FadeCorutine.cs
+++ Assets/Scripts/FadeCorutine.cs
@@ -10,31 +10,40 @@ public class FadeCorutine : MonoBehaviour
     float fadeinTime = 1.5f;
 
     Coroutine coroutine;
+    bool isFadein = true;
+
+    public bool CanStart { get { return coroutine==null||isFadein; } }
 
     private void Awake() {
-        coroutine=StartCoroutine(FadeIn());
+        StartCoroutine(FadeIn());
     }
-    public IEnumerator FadeOut()
-    {
+    public IEnumerator FadeOut() {
+        if (!CanStart) {
+            yield break;
+        }
         if (coroutine != null) {
             StopCoroutine(coroutine);
             coroutine = null;
         }
+        isFadein = false;
+        coroutine = StartCoroutine(FO());
+        yield return coroutine;
+    }
+    public IEnumerator FO() {
 
         Image image = GetComponent<Image>();
-        image.enabled  =  true;
+        image.enabled = true;
         Color color = image.color;
         color.a = 0;
         image.color = color;
 
         yield return new WaitForEndOfFrame();
-        while (true)
-        {
+        while (true) {
             if (color.a > 1) {
                 break;
             }
 
-            color.a += Time.unscaledDeltaTime/fadeoutTime;
+            color.a += Time.unscaledDeltaTime / fadeoutTime;
             image.color = color;
 
             yield return new WaitForEndOfFrame();
@@ -43,23 +52,31 @@ public class FadeCorutine : MonoBehaviour
         coroutine = null;
     }
     public IEnumerator FadeIn() {
+        if (!CanStart) {
+            yield break;
+        }
 
         if (coroutine != null) {
             StopCoroutine(coroutine);
             coroutine = null;
         }
+        coroutine = StartCoroutine(FI());
+        isFadein = true;
+        yield return coroutine;
+    }
+    
+    public IEnumerator FI() {
 
         Image image = GetComponent<Image>();
-        image.enabled  =  true;
+        image.enabled = true;
         Color color = image.color;
         color.a = 1;
         image.color = color;
 
         yield return new WaitForEndOfFrame();
         yield return new WaitForEndOfFrame();
-        while (true)
-        {
-            if (color.a<=0) {
+        while (true) {
+            if (color.a <= 0) {
                 break;
             }
 
