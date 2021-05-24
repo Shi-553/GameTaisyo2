bf3b01d0ed986a8a5933ab69e0f2ba158fd43aa8
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-05-24 23:54:30 +0900

    救済リトライを選べるようにした

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    [SerializeField]
+    ButtonManager buttonManager;
+    [SerializeField]
+    Transform relifeRetry;
+    void Start()
+    {
+        if (StageManager.Instance.IsDeathHpUp()) {
+            var root = buttonManager.transform;
+            var childPos1 = root.GetChild(0).localPosition;
+            childPos1.y -= 150;
+            root.GetChild(0).localPosition= childPos1;
+            var childPos2 = root.GetChild(1).localPosition;
+            childPos2.y -= 130;
+            root.GetChild(1).localPosition = childPos2;
+
+
+            relifeRetry.gameObject.SetActive(true);
+            relifeRetry.SetParent(root);
+            relifeRetry.SetAsFirstSibling();
+
+            buttonManager.ButtonReset();
+        }
+    }
+}
