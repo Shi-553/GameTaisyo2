e8f8aab842e1d218d5bd581194c35b7f84296683
Shi-553 <58556078+Shi-553@users.noreply.github.com>
2021-05-27 02:48:02 +0900

    アニメ追加

---
+++ Assets/SerialNumberPng.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class SerialNumberPng : MonoBehaviour
+{
+    [SerializeField]
+    List<Sprite> sprites;
+    public void Play() {
+        StartCoroutine(Anime());
+    }
+    IEnumerator Anime() {
+        var image = GetComponent<Image>();
+        for (int i = 0; i < sprites.Count; i++) {
+            image.sprite = sprites[i];
+            yield return null;
+        }
+    }
+}
